51e6a24f09f272e4b2d637fbaff8927672c4017b
shougong <shougong@burgeon.cn>
2022-05-30 15:42:14 +0800

    fix build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:'./'
+  publicPath: './',
+  outputDir: "dist", // 输出文件目录
+  lintOnSave: false, // eslint 是否在保存时检查
+  assetsDir: 'static', // 配置js、css静态资源二级目录的位置
+  indexPath: 'index.html',
 })
