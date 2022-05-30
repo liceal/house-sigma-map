d6c239c4fcfbfa07a32770b872e05ef6f78c2d47
shougong <shougong@burgeon.cn>
2022-05-30 15:56:36 +0800

    Revert "fix build"
    This reverts commit 952c9a9449f4b0605cfa8d69f55b3cbad097fdf9.

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: './',
-  outputDir: "dist", // 输出文件目录
-  lintOnSave: false, // eslint 是否在保存时检查
-  assetsDir: 'static', // 配置js、css静态资源二级目录的位置
-  indexPath: 'index.html',
+  transpileDependencies: true
 })
